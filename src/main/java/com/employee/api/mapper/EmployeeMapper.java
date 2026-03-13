2f63f18993dd3bb3dcbfd2042785996ba72ffc9f
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-13 15:51:08 +0900

    EmployeeMapper add

--- src/main/java/com/employee/api/mapper/EmployeeMapper.java
+++ src/main/java/com/employee/api/mapper/EmployeeMapper.java
@@ -4,36 +4,6 @@ import com.employee.api.dto.EmployeeDto;
 import com.employee.api.entity.Employee;
 
 public class EmployeeMapper {
-    /*
-    public static EmployeeDto mapToEmployeeDto(Employee employee){
-        return new EmployeeDto(
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getEmail(),
-                employee.getDepartment().getId()
-        );
-    }
-
-    public static EmployeeDto mapToEmployeeDepartmentDto(Employee employee){
-        return new EmployeeDto(
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getEmail(),
-                DepartmentMapper.mapToDepartmentDto(employee.getDepartment())
-        );
-    }
-
-    public static Employee mapToEmployee(EmployeeDto employeeDto){
-         Employee employee = new Employee();
-         employee.setId(employeeDto.getId());
-         employee.setFirstName(employeeDto.getFirstName());
-         employee.setLastName(employeeDto.getLastName());
-         employee.setEmail(employeeDto.getEmail());
-         return employee;
-    }
-    */
     // Entity -> DTO (ID만 포함)
     public static EmployeeDto mapToEmployeeDto(Employee employee){
         return EmployeeDto.builder()
