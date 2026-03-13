314d7753dc433b2040fb10e35566fdceef628092
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-13 16:58:32 +0900

    DepartmentServiceImpl update

--- src/main/java/com/employee/api/service/DepartmentServiceImpl.java
+++ src/main/java/com/employee/api/service/DepartmentServiceImpl.java
@@ -2,9 +2,11 @@ package com.employee.api.service;
 
 import com.employee.api.dto.DepartmentDto;
 import com.employee.api.entity.Department;
+import com.employee.api.exception.ResourceNotFoundException;
 import com.employee.api.mapper.DepartmentMapper;
 import com.employee.api.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,12 +36,15 @@ public class DepartmentServiceImpl implements DepartmentService{
                 .orElseThrow(() ->
                         new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
         );
+        //Entity => DTO 변환
         return DepartmentMapper.mapToDepartmentDto(department);
          */
         return departmentRepository.findById(departmentId) //Optional<Department>
                 //.map(department -> DepartmentMapper.mapToDepartmentDto(department))
                 .map(DepartmentMapper::mapToDepartmentDto) //Optional<DepartmentDto)
-                .orElseThrow();
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Department is not exists with a given id: " + departmentId,
+                        HttpStatus.NOT_FOUND));
     }
 
     @Transactional(readOnly = true)
