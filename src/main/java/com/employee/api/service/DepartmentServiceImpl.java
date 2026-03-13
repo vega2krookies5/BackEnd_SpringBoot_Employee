cf195c3c90d6dd4848284587ced395709762ceb2
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-13 16:27:04 +0900

    DepartmentServiceImpl update

--- src/main/java/com/employee/api/service/DepartmentServiceImpl.java
+++ src/main/java/com/employee/api/service/DepartmentServiceImpl.java
@@ -1,12 +1,19 @@
 package com.employee.api.service;
 
 import com.employee.api.dto.DepartmentDto;
+import com.employee.api.repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService{
+    private final DepartmentRepository departmentRepository;
+
     @Override
     public DepartmentDto createDepartment(DepartmentDto departmentDto) {
         return null;
