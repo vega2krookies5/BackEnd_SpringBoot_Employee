d4624d12ac9bf12e1fc313925bd68f9654d2d689
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-16 09:57:06 +0900

    EmployeeService update

--- src/main/java/com/employee/api/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/employee/api/service/impl/EmployeeServiceImpl.java
@@ -1,13 +1,50 @@
 package com.employee.api.service.impl;
 
+import com.employee.api.dto.EmployeeDto;
+import com.employee.api.repository.DepartmentRepository;
+import com.employee.api.repository.EmployeeRepository;
 import com.employee.api.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
 
+    private final DepartmentRepository departmentRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        return null;
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        return null;
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        return List.of();
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployeesDepartment() {
+        return List.of();
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+
+    }
 }
