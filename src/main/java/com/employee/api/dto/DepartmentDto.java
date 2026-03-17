d6eddad0a3c76a895adca45078534be12c903fee
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-17 10:54:46 +0900

    dto update

--- src/main/java/com/employee/api/dto/DepartmentDto.java
+++ src/main/java/com/employee/api/dto/DepartmentDto.java
@@ -1,5 +1,6 @@
 package com.employee.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -9,6 +10,10 @@ import lombok.*;
 @Builder
 public class DepartmentDto {
     private Long id;
+
+    @NotBlank(message = "부서 이름은 필수 입력 항목입니다.")
     private String departmentName;
+
+    @NotBlank(message = "부서 설명은 필수 입력 항목입니다.")
     private String departmentDescription;
 }
--- src/main/java/com/employee/api/dto/EmployeeDto.java
+++ src/main/java/com/employee/api/dto/EmployeeDto.java
@@ -1,5 +1,6 @@
 package com.employee.api.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -15,14 +16,16 @@ public class EmployeeDto {
 
     @NotBlank(message = "직원 firstName은 필수 입력 항목입니다.")
     private String firstName;
+
     @NotBlank(message = "직원 lastName은 필수 입력 항목입니다.")
     private String lastName;
 
     @NotBlank(message = "직원 email은 필수 입력 항목입니다.")
+    @Email
     private String email;
 
-    @NotNull(message = "직원의 부서코드는 필수 입력 항목입니다.") // null 방지 (필수)
-    @Positive(message = "올바른 부서 형식이 아닙니다.") // 0 또는 음수 방지
+    @NotNull(message = "직원의 departmentId는 필수 입력 항목입니다.") // null 방지 (필수)
+    @Positive(message = "올바른 부서 코드가 아닙니다.") // 0 또는 음수 방지
     private Long departmentId;
 
     private DepartmentDto departmentDto;
