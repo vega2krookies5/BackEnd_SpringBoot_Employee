aa92604bf0d3a7bf8efe40df4b35b3c35806e69e
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-26 15:17:36 +0900

    SecurityConfig add

---
+++ src/main/java/com/employee/api/auth/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.employee.api.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+
+}
