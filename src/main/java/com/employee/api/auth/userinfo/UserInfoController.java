31ad46df5c558e8db12befde5815775541897b71
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-27 09:28:15 +0900

    UserInfoController login add

--- src/main/java/com/employee/api/auth/userinfo/UserInfoController.java
+++ src/main/java/com/employee/api/auth/userinfo/UserInfoController.java
@@ -1,6 +1,12 @@
 package com.employee.api.auth.userinfo;
 
+import com.employee.api.auth.jwt.AuthRequest;
+import com.employee.api.auth.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserInfoController {
 	@Autowired
 	private UserInfoUserDetailsService service;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
 	
 	@PostMapping("/new")
 	public String addNewUser(@RequestBody UserInfo userInfo){
 		return service.addUser(userInfo);
 	}
+
+    @PostMapping("/login")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getEmail(),
+                        authRequest.getPassword()
+                ));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getEmail());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
 }
