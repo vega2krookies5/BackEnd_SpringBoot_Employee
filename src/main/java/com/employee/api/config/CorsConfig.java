9612fce0b8d45649578b7ec34bf02541c236251c
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-27 14:18:52 +0900

    cors 문제해결

--- src/main/java/com/employee/api/config/CorsConfig.java
+++ src/main/java/com/employee/api/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package net.restapi.emp.config;
+package com.employee.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
